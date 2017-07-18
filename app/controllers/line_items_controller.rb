f6d959244279370e2eb72b0d84fb4968810a3093
lbias <306508354@qq.com>
2017-07-18 23:47:17 +0800

    limit access in controller store, sessions, carts, line_items, orders

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,6 +2,7 @@ class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize, only: :create
 
   # GET /line_items
   # GET /line_items.json
@@ -32,7 +33,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
-        format.js   { @current_item = @line_item }        
+        format.js   { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
